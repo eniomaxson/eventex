cbf44943a46434618e1c33bcda2526d867fee57d
Enio <enio.maxson@gmail.com>
2014-02-03 22:46:33 -0300

    alterando o settings

--- eventex/urls.py
+++ eventex/urls.py
@@ -5,8 +5,7 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    url(r'^$', 'eventex.core.views.home', name='home'),
+    url(r'', include('eventex.core.urls', namespace='core')),
     # url(r'^blog/', include('blog.urls')),
-
     url(r'^admin/', include(admin.site.urls)),
 )
